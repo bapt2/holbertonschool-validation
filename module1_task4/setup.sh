fa92ad1b088d2c530dcaf2cfae457d7b81273082
bapt2 <baptiste18c.touraud@gmail.com>
2023-05-02 18:47:49 +0200

    better file

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -5,5 +5,6 @@ apt-get install wget
 
 wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
 tar -zxvf hugo_extended_0.84.0_Linux-64bit.tar.gz
+mv hugo /usr/local/bin
 
 make build
