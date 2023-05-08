593f33a48229d2fbb17faa74150a091f1158f131
bapt2 <113673417+bapt2@users.noreply.github.com>
2023-05-08 12:07:24 +0200

    remove awesome-api

--- module2_task0/main.go
+++ module2_task0/main.go
@@ -25,7 +25,7 @@ func setupRouter() *mux.Router {
   // Create a new empty HTTP Router
   r := mux.NewRouter()
 
-  // When an HTTP GET request is received on the path /health, delegates to the function "HealthCheckHandler()"
+  // When an HTTP GET request is received on the path /health, delegates to the function "HealthCheckHandler() "
   r.HandleFunc("/health", HealthCheckHandler).Methods("GET")
 
   return r
