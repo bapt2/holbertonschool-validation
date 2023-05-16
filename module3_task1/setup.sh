d14003b33cfaba2594e7a4e5acf7ba0c1a8bf685
bapt2 <113673417+bapt2@users.noreply.github.com>
2023-05-16 10:25:49 +0200

    test7

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get install -y make curl
-# Download Hugo binary
-HUGO_VERSION="0.84.0"
-HUGO_BINARY="hugo_extended_${HUGO_VERSION}_Linux-64bit.deb"
-curl -LO "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_BINARY}"
-sudo dpkg -i ${HUGO_BINARY}
-rm ${HUGO_BINARY}
+# Install make and wget
+sudo apt-get update && apt-get install -y make wget
 
+# Install v0.84.0 of Hugo
+wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
+sudo tar -zxvf hugo_extended_0.84.0_Linux-64bit.tar.gz
+mv hugo /usr/local/bin
 
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s v1.42.1
+# Install Golang v1.15.14
+wget https://golang.org/dl/go1.15.14.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.15.14.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+source ~/.profile
+
+# Install nodejs v14.* and npm v7*
+sudo apt-get install -y nodejs=14.21.3-deb-1nodesource1
+sudo npm install -g npm@7
+
+# Install binaries golangci-lint
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s 
 sudo mv golangci-lint-*/golangci-lint /usr/local/bin/
 
-sudo dpkg -i /tmp/golangci-lint.deb
-rm -f /tmp/golangci-lint.deb
-sudo npm install --global \
-  markdownlint-cli@0.26.0 \
-  markdown-link-check@3.8.6
+# Install markdownlint-cli and 
+sudo npm install -g markdownlint-cli
+sudo npm install -g markdown-link-check
