d79d42cd16618f5e0e061d3b9022804f3f201018
bapt2 <113673417+bapt2@users.noreply.github.com>
2023-05-18 12:14:06 +0200

    test 5

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,23 +1,23 @@
 # Install make and wget
-sudo apt-get update && apt-get install -y make wget curl
+apt-get update && apt-get install -y make wget curl
 
 # Install Hugo v0.84.0
 wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
-sudo tar -zxvf hugo_extended_0.84.0_Linux-64bit.tar.gz
+tar -zxvf hugo_extended_0.84.0_Linux-64bit.tar.gz
 mv hugo /usr/local/bin
 
 # Install golangci-lint
 curl --silent --show-error --location --output /tmp/golangci-lint.deb \
      "https://github.com/golangci/golangci-lint/releases/download/v1.52.2/golangci-lint-1.52.2-linux-amd64.deb"
-sudo dpkg -i /tmp/golangci-lint.deb
+dpkg -i /tmp/golangci-lint.deb
 rm -f /tmp/golangci-lint.deb
 
 # Install nodejs v14.* and npm v7*
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo npm install -g npm@7
+apt-get install -y nodejs
+npm install -g npm@7
 
 # Install markdownlint-cli and 
-sudo npm install -g markdownlint-cli
-sudo npm install -g markdown-link-check
+npm install -g markdownlint-cli
+npm install -g markdown-link-check
   
