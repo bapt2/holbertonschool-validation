81676b9f36f667a62e716e30e84303f54bcc2ada
bapt2 <113673417+bapt2@users.noreply.github.com>
2023-05-18 12:33:31 +0200

    test 8

--- module4_task0/go.mod
+++ module4_task0/go.mod
@@ -2,7 +2,4 @@ module github.com/Lestt/awesome-api
 
 go 1.20
 
-require (
-	github.com/google/subcommands v1.2.0
-	github.com/gorilla/mux v1.8.0
-)
+require github.com/gorilla/mux v1.8.0
--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,5 +1,5 @@
 # Install make and wget
-apt-get update && apt-get install -y make wget curl
+apt-get update && sudo apt-get install -y make wget curl
 
 # Install Hugo v0.84.0
 wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
@@ -13,11 +13,11 @@ dpkg -i /tmp/golangci-lint.deb
 rm -f /tmp/golangci-lint.deb
 
 # Install nodejs v14.* and npm v7*
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-apt-get install -y nodejs
+curl -sL https://deb.nodesource.com/setup_14.x | bash -
+apt-get install -y --no-install-recommends nodejs=14.*
 npm install -g npm@7
 
 # Install markdownlint-cli and 
 npm install --global \
-  markdownlint-cli \ 
-  markdown-link-check
+  markdownlint-cli@0.26.0 \
+  markdown-link-check@3.8.6
